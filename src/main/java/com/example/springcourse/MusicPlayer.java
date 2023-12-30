f0d6aecaa203daccdf7f39034163f6e5b3b2ce66
Sergey Fomenko <artem.work749@gmail.com>
2023-12-30 09:54:14 +0300

    Lesson 5 - Dependency Injection part 1

--- src/main/java/com/example/springcourse/MusicPlayer.java
+++ src/main/java/com/example/springcourse/MusicPlayer.java
@@ -3,6 +3,8 @@ package com.example.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {}
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
